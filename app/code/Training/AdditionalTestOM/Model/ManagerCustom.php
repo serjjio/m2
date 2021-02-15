1102ddac8a9570ba3a03e5c179f77c20bc9498b2
Serg <sergey.plushch@gmail.com>
2021-02-15 13:02:20 +0200

    Task 1.3

---
+++ app/code/Training/AdditionalTestOM/Model/ManagerCustom.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Training\AdditionalTestOM\Model;
+
+use Training\TestOM\Model\ManagerInterface;
+
+class ManagerCustom implements ManagerInterface
+{
+
+    public function create()
+    {
+
+    }
+
+    public function get()
+    {
+
+    }
+}
