308799cb21abc1236cc54801a5a81500b939f64b
serjjio <vvirus@ukr.net>
2021-02-23 08:55:05 +0200

    Task 2.2

---
+++ app/code/Training/Test/App/FrontController.php
@@ -0,0 +1,33 @@
+<?php
+namespace Training\Test\App;
+
+class FrontController extends \Magento\Framework\App\FrontController
+{
+    /**
+     * @var \Psr\Log\LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @param \Magento\Framework\App\RouterListInterface $routerList
+     * @param \Magento\Framework\App\ResponseInterface $response
+     * @param \Psr\Log\LoggerInterface $logger
+     */
+    public function __construct(
+        \Magento\Framework\App\RouterListInterface $routerList,
+        \Magento\Framework\App\ResponseInterface $response,
+        \Psr\Log\LoggerInterface $logger
+    ) {
+        $this->logger = $logger;
+        parent::__construct($routerList, $response);
+    }
+
+    public function dispatch(\Magento\Framework\App\RequestInterface $request)
+    {
+        foreach ($this->_routerList as $router) {
+            $this->logger->info(get_class($router));
+        }
+
+        return parent::dispatch($request);
+    }
+}
