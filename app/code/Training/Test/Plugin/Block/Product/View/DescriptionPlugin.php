e268150edf8bc09f672878f3088354315b4fa9a4
Serg <sergey.plushch@gmail.com>
2021-02-25 17:00:03 +0200

    Task 3.8

--- app/code/Training/Test/Plugin/Block/Product/View/DescriptionPlugin.php
+++ app/code/Training/Test/Plugin/Block/Product/View/DescriptionPlugin.php
@@ -6,7 +6,15 @@ class DescriptionPlugin
     public function beforeToHtml(
         \Magento\Catalog\Block\Product\View\Description $subject
     ) {
+        /*Task 3.4*/
         //$subject->getProduct()->setData('description', 'Test Description');
-        $subject->setTemplate('Training_Test::description.phtml');
+
+        /*Task 3.7*/
+        //$subject->setTemplate('Training_Test::description.phtml');
+
+        /*Task 3.8*/
+        if ($subject->getNameInLayout() == 'product.info.sku') {
+            $subject->setTemplate('Training_Test::description.phtml');
+        }
     }
 }
