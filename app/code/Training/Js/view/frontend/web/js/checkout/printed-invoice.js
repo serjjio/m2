d579e0bf3542f6146027b1b8f7e02ebbcdd8e868
Serg <sergey.plushch@gmail.com>
2021-03-12 13:23:47 +0200

    Task 4.3

---
+++ app/code/Training/Js/view/frontend/web/js/checkout/printed-invoice.js
@@ -0,0 +1,14 @@
+define([
+    'uiComponent',
+    'jquery',
+    'ko'
+], function (Component, $, ko) {
+    'use strict';
+    return Component.extend({
+
+        initialize: function () {
+            this._super();
+        },
+
+    });
+});
