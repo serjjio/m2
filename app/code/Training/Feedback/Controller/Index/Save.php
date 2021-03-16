908b70026fe732b3e1345415acc46cda3c0d48b3
Serg <sergey.plushch@gmail.com>
2021-03-16 12:30:19 +0200

    Task 5.1

---
+++ app/code/Training/Feedback/Controller/Index/Save.php
@@ -0,0 +1,60 @@
+<?php
+declare(strict_types=1);
+
+namespace Training\Feedback\Controller\Index;
+
+class Save extends \Magento\Framework\App\Action\Action
+{
+    private $feedbackFactory;
+
+    private $feedbackResource;
+
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Training\Feedback\Model\FeedbackFactory $feedbackFactory,
+        \Training\Feedback\Model\ResourceModel\Feedback $feedbackResource
+    ) {
+        $this->feedbackFactory = $feedbackFactory;
+        $this->feedbackResource = $feedbackResource;
+        parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        $result = $this->resultRedirectFactory->create();
+        if ($post = $this->getRequest()->getPostValue()) {
+            try {
+                $this->validatePost($post);
+                $feedback = $this->feedbackFactory->create();
+                $feedback->setData($post);
+                $this->messageManager->addSuccessMessage(
+                    __('Thank you for your feedback.')
+                );
+            } catch (\Exception $e) {
+                $this->messageManager->addErrorMessage(
+                    __('An error occurred while processing your form. Please try again later.')
+                );
+                $result->setPath('*/*/form');
+                return $result;
+            }
+        }
+        return $result->setPath('*/*/index');
+
+    }
+
+    private function validatePost($post)
+    {
+        if (!isset($post['author_name']) || trim($post['author_name']) === '') {
+            throw new LocalizedException(__('Name is missing'));
+        }
+        if (!isset($post['message']) || trim($post['message']) === '') {
+            throw new LocalizedException(__('Comment is missing'));
+        }
+        if (!isset($post['author_email']) || false === \strpos($post['author_email'], '@')) {
+            throw new LocalizedException(__('Invalid email address'));
+        }
+        if (trim($this->getRequest()->getParam('hideit')) !== '') {
+            throw new \Exception();
+        }
+    }
+}
---
+++ app/code/Training/Feedback/Model/ResourceModel/Feedback/Collection.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace Training\Feedback\Model\ResourceModel\Feedback;
+
+use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
+
+class Collection extends AbstractCollection
+{
+    /**
+     * @return void
+     */
+    protected function _construct()
+    {
+        $this->_init(
+            \Training\Feedback\Model\Feedback::class,
+            \Training\Feedback\Model\ResourceModel\Feedback::class
+        );
+    }
+}
